84626ed407e5bc5771c804107356901a75747f61
桂川直也 <katsuragawanaoya@Hajimari-NaoyaKatsuragawa.local>
2021-08-15 00:03:48 +0900

    tool install

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+import styled from 'styled-components'
+
+const Header = styled.h1`
+    color: red;
+  `
+
+const Main = (<Header>Markdown Editor</Header>)
+
+render(Main, document.getElementById('app'))
